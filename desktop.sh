66c8504d2dca0f31cb5052bf88ca7904f59a3325
IconsNotFound <76126001+IconsNotFound@users.noreply.github.com>
2022-07-31 22:45:27 +0600

    Add files via upload

---
+++ desktop.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+gsettings set org.gnome.desktop.interface clock-format 12h
+gsettings set org.gnome.desktop.interface clock-show-weekday true
+gsettings set org.gnome.desktop.interface icon-theme 'Yaru-prussiangreen'
+gsettings set org.gnome.desktop.interface gtk-theme 'Yaru-prussiangreen-dark'
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+gsettings set org.gnome.desktop.interface show-battery-percentage true
---
+++ essential-apps.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo apt install gnome-tweaks -y
+sudo apt install gnome-shell-extensions -y
